6eff0895a769251e5fc089269e3a7f69ec105523
Yonguk Jeong <yonguk@nyu.edu>
2019-10-08 01:22:35 -0400

    Login and redirect user

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
 
 
 class LoginView(TemplateView):
     template_name = "login.html"
 
     def post(self, request, *args, **kwargs):
-        raise Http404("not implemented")
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+
+        else:
+            return HttpResponseRedirect(reverse('login'))
 
 
 def index(request):
