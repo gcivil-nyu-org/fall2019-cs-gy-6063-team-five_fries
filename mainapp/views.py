b51db3b13ddc9dd1b80dbc5412924251c31b37db
Yonguk Jeong <yonguk@nyu.edu>
2019-10-08 00:53:52 -0400

    Send form to POST /login

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+from django.http import Http404
+
+
+class LoginView(TemplateView):
+    template_name = "login.html"
+
+    def post(self, request, *args, **kwargs):
+        raise Http404("not implemented")
 
 
 def index(request):
     return render(request, 'index.html')
-
-def login(request):
-    return render(request, 'login.html')
