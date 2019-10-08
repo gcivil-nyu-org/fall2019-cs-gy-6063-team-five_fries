9c9456e45e7983cb8d99400f1c964ddced0affeb
Alex Biehl <ab7289@nyu.edu>
2019-10-08 19:37:29 -0400

    initial setup for bootstrap crispy-forms

--- citystreets/settings.py
+++ citystreets/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Crispy_Forms settings
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Activating Django Heroku
 
