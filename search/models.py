8ddca3001773dbf4bba5d9a8d6bcfb9b6c0472bd
Alex Biehl <ab7289@nyu.edu>
2019-10-12 15:39:25 -0400

    Added django-craigslist to requirements.txt and created base model to store the objects

--- search/models.py
+++ search/models.py
@@ -1,3 +1,15 @@
 # from django.db import models
 
 # Create your models here.
+
+class CraigslistLocation(models.Model):
+    c_id = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    date_time = models.DateTimeField()
+    url = models.URLField()
+    where = models.CharField(max_length=200)
+    has_image = models.BooleanField(default=False)
+    has_map = models.BooleanField(default=False)
+    lat = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+    lon = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,36 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.utils import timezone 
+
+import datetime
+from .models import CraigslistLocation
+
 
 # Create your tests here.
+
+
+def create_c_location(id, name, url, date_time, days, price, where, 
+    has_image, has_map, lat, lon):
+    '''
+    Creates a CraigslistLocation with the given values
+    and datetime the given number of 'days' offset to now
+    (negative for times in the past, positive for times in
+    the future)
+    '''
+    timediff = 0 if (days is None) else days
+    time = timezone.now() + datetime.timedelta(days=timediff)
+    # Used ternary so it's not necessary to fill in every field
+    # each time a location is created.
+    return CraigslistLocation.objects.create(
+        c_id=('1234' if (id is None) else id),
+        name=('A cool place to live!' if (name is None) else name),
+        url=('http://a-url.com' if (url is None) else url),
+        date_time=time,
+        price=('$3.50' if (price is None) else price),
+        where=('Sunnyside Heights' if (where is None) else where),
+        has_image=(False if (has_image is None) else has_image),
+        has_map=(False if (has_map is None) else has_map),
+        # coordinates for some random spot in Central Park
+        lat=(40.772480 if (lat is None) else lat),
+        lon=(-73.972580 if (lon is None) else lon)
+    )
+
