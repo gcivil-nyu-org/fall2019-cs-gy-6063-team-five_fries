51341f55197a8722ea56b9ec0ec841652318e276
Ayushi-0903 <ag7335@nyu.edu>
2019-10-16 10:01:01 -0400

    User story #97

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.LoginView.as_view(), name="login"),
     path("account", views.account, name="account"),
+    path('Search', views.search, name='search'),
 ]
