669725b456d265eb75a74fafe466e3e5b441d97f
Yonguk Jeong <yonguk@nyu.edu>
2019-10-16 10:23:23 -0400

    Do not specify which form to use in the profile admin page

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 
-from signup.forms import SignUpForm
 from .models import Profile
 
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ["__str__", "timestamp", "updated"]
-    form = SignUpForm
 
 
 admin.site.register(Profile, ProfileAdmin)
