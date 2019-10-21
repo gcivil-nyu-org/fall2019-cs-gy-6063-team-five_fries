3190024c5e7715530ecf80b25c0aaac6ebdfebf2
KShih <kontonsine@gmail.com>
2019-10-21 09:57:29 -0400

    display craigslist data on map

--- map/models.py
+++ map/models.py
@@ -4,6 +4,7 @@ from djgeojson.fields import PointField
 
 class CityStreetSpot(models.Model):
 
+    c_id = models.CharField(max_length=200)
     title = models.CharField(max_length=256)
     description = models.TextField()
     picture = models.ImageField(upload_to="./map/static/map")
--- map/views.py
+++ map/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
-
+from search.models import CraigslistLocation
+from map.models import CityStreetSpot
 
 def mapview(request):
+    craigslist_datas = CraigslistLocation.objects.all()
+    marker = CityStreetSpot
+    for data in craigslist_datas:
+        if not CityStreetSpot.objects.filter(c_id = data.c_id).exists():
+            marker = CityStreetSpot()
+            title = data.name
+            description = data.price + "\n" + str(data.date_time) + "\n" + data.url
+
+            # TODO: fetch img from data.url
+            image_url = "no_apa_pic.png"
+
+            geom = dict({'type': 'Point', 'coordinates': [data.lon, data.lat]})
+
+            q = CityStreetSpot(
+                c_id = data.c_id,
+                title = title,
+                description = description,
+                picture = image_url,
+                geom = geom
+            )
+            q.save()
+        # TODO: UPDATE data, if exist but had updating crag data
+
     return render(request, "map.html")
