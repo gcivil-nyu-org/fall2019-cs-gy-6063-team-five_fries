6c8b1fd00bcb218478d3d12fc477b28556885032
Ayushi-0903 <ag7335@nyu.edu>
2019-10-22 10:07:16 -0400

    Restraunts Data

--- search/views.py
+++ search/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from django.utils import timezone
-
 from craigslist import CraigslistHousing
-
 from .GetRentalHouse import get_rental_house
 from .forms import ZillowSearchForm
 from .models import CraigslistLocation, LastRetrievedData
 import json
+import requests
+
 from data_311.get_311_data import get_311_data
 from data_311.get_311_statistics import get_311_statistics
 
@@ -119,3 +119,17 @@ def clist_results(request):
     result_list = CraigslistLocation.objects.all()
     context = {"clist_results": result_list}
     return render(request, "search/clist_results.html", context)
+
+
+
+def restraunts(request):
+    data_restraunts= requests.get("https://data.cityofnewyork.us/resource/43nn-pn8j.json?grade=A")
+    def jprint(obj):
+        # create a formatted string of the Python JSON object
+        text = json.dumps(obj, sort_keys=True, indent=4)
+        print(text)
+
+    jprint(data_restraunts.json())
+    data_restraunts = data_restraunts.text
+    return render(request, "search/data_restraunts.html", {})
+
