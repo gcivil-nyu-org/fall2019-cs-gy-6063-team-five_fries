b54b1f9ea4fc9de26fac44d09ed1db8d0d59aff2
KShih <kontonsine@gmail.com>
2019-10-21 20:50:58 -0400

    file modification by black

--- map/views.py
+++ map/views.py
@@ -2,45 +2,45 @@ from django.shortcuts import render
 from search.models import CraigslistLocation
 from map.models import CityStreetSpot
 
+
 def mapview(request):
     craigslist_datas = CraigslistLocation.objects.all()
     citystreet_obj = CityStreetSpot.objects
 
     for data in craigslist_datas:
-        if not citystreet_obj.filter(c_id = data.c_id).exists():
+        if not citystreet_obj.filter(c_id=data.c_id).exists():
             title = data.name
             description = data.price + "\n" + str(data.date_time) + "\n" + data.url
 
             # TODO: fetch img from data.url
             image_url = "no_apa_pic.png"
 
-            geom = dict({'type': 'Point', 'coordinates': [data.lon, data.lat]})
+            geom = dict({"type": "Point", "coordinates": [data.lon, data.lat]})
 
             q = CityStreetSpot(
-                c_id = data.c_id,
-                title = title,
-                description = description,
-                last_update = data.date_time,
-                picture = image_url,
-                geom = geom
-            )
-            q.save()
-        elif data.date_time != citystreet_obj.get(c_id = data.c_id).last_update:
-            title = data.name
-            description = data.price + "\n" + str(data.date_time) + "\n" + data.url
-
-            # TODO: fetch img from data.url
-            image_url = "no_apa_pic.png"
-
-            geom = dict({'type': 'Point', 'coordinates': [data.lon, data.lat]})
-
-            citystreet_obj.filter(c_id = data.c_id).update(
+                c_id=data.c_id,
                 title=title,
                 description=description,
                 last_update=data.date_time,
                 picture=image_url,
-                geom=geom
+                geom=geom,
             )
+            q.save()
+        elif data.date_time != citystreet_obj.get(c_id=data.c_id).last_update:
+            title = data.name
+            description = data.price + "\n" + str(data.date_time) + "\n" + data.url
 
+            # TODO: fetch img from data.url
+            image_url = "no_apa_pic.png"
+
+            geom = dict({"type": "Point", "coordinates": [data.lon, data.lat]})
+
+            citystreet_obj.filter(c_id=data.c_id).update(
+                title=title,
+                description=description,
+                last_update=data.date_time,
+                picture=image_url,
+                geom=geom,
+            )
 
     return render(request, "map.html")
