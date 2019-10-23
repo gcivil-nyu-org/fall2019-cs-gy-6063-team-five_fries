82596e68f8f491396ab10fa8f352f7292b8d3c11
Yonguk Jeong <yonguk@nyu.edu>
2019-10-23 11:42:25 -0400

    Add a testcase for CityStreetSpot

--- map/tests.py
+++ map/tests.py
@@ -1,3 +1,11 @@
-# from django.test import TestCase
+from django.test import TestCase
+from .models import CityStreetSpot
 
-# Create your tests here.
+
+class CityStreetSpotTestCase(TestCase):
+    def test_picture_url_under_static(self):
+        """picture_url should be a url under /static/map/filename"""
+        spot = CityStreetSpot.objects.create(
+            last_update="2019-10-23 11:21", picture="image.jpg"
+        )
+        self.assertEqual(spot.picture_url, "/static/map/image.jpg")
