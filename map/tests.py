f5eb5b77d1d641cf6515b197ca32106f84acc18c
Yonguk Jeong <yonguk@nyu.edu>
2019-10-23 14:14:11 -0400

    Add a testcases for `/map`

--- map/tests.py
+++ map/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import CityStreetSpot
+from django.urls import reverse
 
 
 class CityStreetSpotTestCase(TestCase):
@@ -9,3 +10,9 @@ class CityStreetSpotTestCase(TestCase):
             last_update="2019-10-23 11:21", picture="image.jpg"
         )
         self.assertEqual(spot.picture_url, "/static/map/image.jpg")
+
+
+class MapIndexViewTestcase(TestCase):
+    def test_map_index(self):
+        response = self.client.get(reverse("mapview"))
+        self.assertEqual(response.status_code, 200)
