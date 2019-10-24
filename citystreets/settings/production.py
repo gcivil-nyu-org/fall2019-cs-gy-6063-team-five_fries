85d1dc5ac7d1183268da89ada019124aca61e964
ab7289 <ab7289@nyu.edu>
2019-10-24 15:06:28 -0400

    added production email settings

--- citystreets/settings/production.py
+++ citystreets/settings/production.py
@@ -5,3 +5,10 @@ DEBUG = False
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]  # noqa: F405
 
 ALLOWED_HOSTS = ["develop-branch.herokuapp.com/", "master-branch.herokuapp.com/"]
+
+# Email information
+EMAIL_USE_TLS = True
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "team.five.fries@gmail.com"
+EMAIL_HOST_PASSWORD = "90d#C0Yx4EHxh4!mN4er1Z7"
+EMAIL_PORT = 587
