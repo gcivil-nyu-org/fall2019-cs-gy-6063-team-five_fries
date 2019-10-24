70ac38bb011551e3850f351ff4fb64789b245dd2
ab7289 <ab7289@nyu.edu>
2019-10-24 15:10:09 -0400

    replaced email settings with environment vars

--- citystreets/settings/production.py
+++ citystreets/settings/production.py
@@ -9,6 +9,6 @@ ALLOWED_HOSTS = ["develop-branch.herokuapp.com/", "master-branch.herokuapp.com/"
 # Email information
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "team.five.fries@gmail.com"
-EMAIL_HOST_PASSWORD = "90d#C0Yx4EHxh4!mN4er1Z7"
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 EMAIL_PORT = 587
