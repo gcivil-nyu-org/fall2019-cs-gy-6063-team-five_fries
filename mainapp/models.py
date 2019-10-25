18bab1603196d8441f8f7b08d8c5cac48221a89a
ab7289 <ab7289@nyu.edu>
2019-10-25 11:35:49 -0400

    updated html hierarchy and removed redundant base page

--- mainapp/models.py
+++ mainapp/models.py
@@ -9,7 +9,7 @@ class SiteUser(AbstractUser):
     current_location = models.CharField(max_length=255, default="")
     work_location = models.CharField(max_length=255, default="")
 
-    user_type_string_map = {"T": "Tenant", "R": "Renter", "L": "Landlord"}  # noqa F821
+    user_type_string_map = {"T": "Tenant", "R": "Renter", "L": "Landlord"}
     user_type = models.CharField(
         max_length=2, choices=user_type_string_map.items(), default="R"
     )
@@ -17,8 +17,8 @@ class SiteUser(AbstractUser):
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
-    def user_type_string(self):  # noqa F821
-        return self.user_type_string_map.get(self.user_type, "")  # noqa F821
+    def user_type_string(self):
+        return self.user_type_string_map.get(self.user_type, "")
 
     def __str__(self):
         return self.username
