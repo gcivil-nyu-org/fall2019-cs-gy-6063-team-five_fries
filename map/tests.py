083ab7300946bb7a10c0a6190385c2cc60a67111
KShih <kontonsine@gmail.com>
2019-10-25 15:41:42 -0400

    fix the depending foreign key problem in map/test.py

--- map/tests.py
+++ map/tests.py
@@ -1,13 +1,21 @@
 from django.test import TestCase
 from .models import CityStreetSpot
 from django.urls import reverse
+from search.models import CraigslistLocation
 
 
 class CityStreetSpotTestCase(TestCase):
     def test_picture_url_under_static(self):
+        CraigslistLocation.objects.create(
+            c_id="0000000000000000000", date_time="2019-10-23 11:21"
+        )
         """picture_url should be a url under /static/map/filename"""
         spot = CityStreetSpot.objects.create(
-            last_update="2019-10-23 11:21", picture="image.jpg"
+            c_id=CraigslistLocation.objects.only("c_id").get(
+                c_id="0000000000000000000"
+            ),
+            last_update="2019-10-23 11:21",
+            picture="image.jpg",
         )
         self.assertEqual(spot.picture_url, "/static/map/image.jpg")
 
