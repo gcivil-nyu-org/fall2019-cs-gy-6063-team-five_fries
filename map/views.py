5cddd3b5551a4f499d7c8f19711af6d6ebce05e7
KShih <kontonsine@gmail.com>
2019-10-25 15:51:46 -0400

    delete redundant comment

--- map/views.py
+++ map/views.py
@@ -7,8 +7,6 @@ def mapview(request):
     craigslist_datas = CraigslistLocation.objects.all()
 
     for data in craigslist_datas:
-        # TODO: set CityStreetSpot.c_id as a foreignkey from CraigslistLocation
-        # if not citystreet_obj.filter(c_id=data.c_id).exists():
         title = data.name
         url = '<a href="' + data.url + '" target="_blank">' + data.url + "</a>"
         description = data.price + "\n" + str(data.date_time) + "\n" + url
@@ -27,22 +25,5 @@ def mapview(request):
             geom=geom,
         )
         q.save()
-        # elif data.date_time != citystreet_obj.get(c_id=data.c_id).last_update:
-        #     title = data.name
-        #     url = '<a href="' + data.url + '">' + data.url + "</a>"
-        #     description = data.price + "\n" + str(data.date_time) + "\n" + url
-        #
-        #     # TODO: fetch img from data.url
-        #     image_url = "no_apa_pic.jpg"
-        #
-        #     geom = dict({"type": "Point", "coordinates": [data.lon, data.lat]})
-        #
-        #     citystreet_obj.filter(c_id=data.c_id).update(
-        #         title=title,
-        #         description=description,
-        #         last_update=data.date_time,
-        #         picture=image_url,
-        #         geom=geom,
-        #     )
 
     return render(request, "map.html")
