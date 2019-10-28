670cd0c4d5ed4b30f5f9a131e58931b825ca1584
Yonguk Jeong <yonguk@nyu.edu>
2019-10-28 18:00:17 -0400

    Update tests

--- external/craigslist/tests.py
+++ external/craigslist/tests.py
@@ -1,26 +1,23 @@
 from django.test import TestCase
 from .fetch import fetch_craigslist_housing
 import numbers
+from unittest import mock
 
 
 class CraigslistTests(TestCase):
-    def test_fetch(self):
-        results = fetch_craigslist_housing(
+    @mock.patch("external.craigslist.fetch.CraigslistHousing")
+    def test_fetch(self, MockCraigslistHousing):
+        _ = fetch_craigslist_housing(
             limit=3,
             site="newyork",
             category="apa",
             area="brk",
             filters={"max_price": 2000},
         )
-
-        for r in results:
-            if r["geotag"] is not None:
-                self.assertTrue(len(r["geotag"]) >= 2)
-                for p in r["geotag"]:
-                    self.assertTrue(isinstance(p, numbers.Number))
-            self.assertTrue("id" in r)
-            self.assertTrue("name" in r)
-            self.assertTrue("url" in r)
-            self.assertTrue("datetime" in r)
-            self.assertTrue("price" in r)
-            self.assertTrue("has_image" in r)
+        MockCraigslistHousing.assert_called_with(
+            site="newyork",
+            category="apa",
+            area="brk",
+            filters={"max_price": 2000},
+        )
+        MockCraigslistHousing().get_results.assert_called_with(geotagged=True, limit=3)
