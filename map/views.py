afa73c1b3eb37a1e7eca59d42ef7cead98bf56ef
KShih <@gmail.com>
2019-10-28 14:18:25 -0400

    display apartment picture fetching from its craiglist url

--- map/views.py
+++ map/views.py
@@ -10,24 +10,47 @@ def mapview(request):
     craigslist_datas = CraigslistLocation.objects.all()
 
     for data in craigslist_datas:
-        title = data.name
-        url = '<a href="' + data.url + '" target="_blank">' + data.url + "</a>"
-        description = data.price + "\n" + str(data.date_time) + "\n" + url
+        if not CityStreetSpot.objects.filter(c_id=data.c_id).exists():
+            title = data.name
+            url = '<a href="' + data.url + '" target="_blank">' + data.url + "</a>"
+            description = data.price + "\n" + str(data.date_time) + "\n" + url
 
-        # TODO: fetch img from data.url
-        image_url = "../static/map/no_apa_pic.jpg"
+            if data.has_image:
+                image_url = get_img_url(data.url)
+            else:
+                image_url = "../static/map/no_apa_pic.jpg"
 
-        geom = dict({"type": "Point", "coordinates": [data.lon, data.lat]})
+            geom = dict({"type": "Point", "coordinates": [data.lon, data.lat]})
 
-        q = CityStreetSpot(
-            c_id=CraigslistLocation.objects.only("c_id").get(c_id=data.c_id),
-            title=title,
-            description=description,
-            last_update=data.date_time,
-            picture=image_url,
-            geom=geom,
-        )
-        q.save()
+            q = CityStreetSpot(
+                c_id=CraigslistLocation.objects.only("c_id").get(c_id=data.c_id),
+                title=title,
+                description=description,
+                last_update=data.date_time,
+                picture=image_url,
+                geom=geom,
+            )
+            q.save()
+
+        elif data.date_time != CityStreetSpot.objects.get(c_id=data.c_id).last_update:
+            title = data.name
+            url = '<a href="' + data.url + '" target="_blank">' + data.url + "</a>"
+            description = data.price + "\n" + str(data.date_time) + "\n" + url
+
+            if data.has_image:
+                image_url = get_img_url(data.url)
+            else:
+                image_url = "../static/map/no_apa_pic.jpg"
+
+            geom = dict({"type": "Point", "coordinates": [data.lon, data.lat]})
+
+            CityStreetSpot.filter(c_id = data.c_id).update(
+                title=title,
+                description=description,
+                last_update=data.date_time,
+                picture=image_url,
+                geom=geom,
+            )
     return render(request, "map.html")
 
 def get_img_url(url):
