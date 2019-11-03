12bda1f874a683581b78542e19f75296e5bb98cd
muhammad_osama_khan <mok232@nyu.edu>
2019-11-03 17:18:57 -0500

    created favlist.html to display favorites list and created views.py to gather the favorited apartments

--- mainapp/views.py
+++ mainapp/views.py
@@ -9,3 +9,8 @@ def index(request):
 @login_required
 def account(request):
     return render(request, "account.html", {"user": request.user})
+
+@login_required
+def favlist(request):
+    favorited_apartments = request.user.favorites.all()
+    return render(request, "favlist.html", {"favorited_apartments": favorited_apartments})
