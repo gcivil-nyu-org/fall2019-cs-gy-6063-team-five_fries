4e93e90bc5a87cb7997c8dd6e9737a945bdd96da
muhammad_osama_khan <mok232@nyu.edu>
2019-11-03 00:01:54 -0400

    added many-to-many field to siteuser model to add favorited apartments

--- location/urls.py
+++ location/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("<int:pk>/", views.LocationView.as_view(), name="location")]
+# urlpatterns = [path("<int:pk>/", views.LocationView.as_view(), name="location"),
+#                ]
+
+urlpatterns = [path("<int:pk>/", views.favorites, name="location"),
+               ]
--- location/views.py
+++ location/views.py
@@ -1,6 +1,10 @@
 from django.views import generic
 from .models import Location
 from external.cache.zillow import refresh_zillow_housing_if_needed
+from mainapp.models import SiteUser
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 class LocationView(generic.DetailView):
@@ -11,3 +15,15 @@ class LocationView(generic.DetailView):
         obj = super().get_object()
         refresh_zillow_housing_if_needed(obj)
         return obj
+def favorites(request, pk):
+
+    apartment = get_object_or_404(Location, pk = pk)
+    print("print")
+    # user = get_object_or_404(SiteUser, pk= user)
+    # user.favorites.add(apartment)
+    # favorited_apartments = user.favorites.all()
+    # print("Apartment selected as favorite!!!")
+    # return render(request, reverse("location", args=(1,), {"favorited_apartments": favorited_apartments}))
+    # return render(request, reverse("location", args=pk), {})
+    # return render(reverse("location", args=(pk,)), {})
+    return render(request, reverse("location", args=(pk,)), {})
