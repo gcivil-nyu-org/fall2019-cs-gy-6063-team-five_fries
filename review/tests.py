cd29bf43786e78d52892d04466d0e1713b8c6634
KShih <kontonsine@gmail.com>
2019-11-04 17:33:06 -0500

    fix flack8 and black

--- review/tests.py
+++ review/tests.py
@@ -8,13 +8,13 @@ from datetime import datetime
 class ReviewModelTests(TestCase):
 
     def test_create_model(self):
-        s1 = SiteUser.objects.create(full_name = "test_user")
-        l1 = Location.objects.create(state = "NY")
+        s1 = SiteUser.objects.create(full_name="test_user")
+        l1 = Location.objects.create(state="NY")
         rev = Review.objects.create(location=l1, user=s1, content="test_content", time=datetime.now())
         self.assertIsNotNone(rev)
 
     def test_add_review(self):
-        s1 = SiteUser.objects.create(full_name = "test_user")
-        l1 = Location.objects.create(state = "NY")
+        s1 = SiteUser.objects.create(full_name="test_user")
+        l1 = Location.objects.create(state="NY")
         r1 = Review.objects.create(location=l1, user=s1, content="test_content", time=datetime.now())
         self.assertEqual(r1.content, "test_content")
