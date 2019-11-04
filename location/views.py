3e87f0d2d615790640a97e78e4d9a175b614e467
KShih <kontonsine@gmail.com>
2019-11-04 16:11:59 -0500

    Able to write commit on the location/pk/

--- location/views.py
+++ location/views.py
@@ -1,4 +1,10 @@
 from django.views import generic
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+from review.models import Review
+from review.form import ReviewForm
 from .models import Location
 from external.cache.zillow import refresh_zillow_housing_if_needed
 from django.shortcuts import render
@@ -40,3 +46,22 @@ def favlist(request):
     return render(
         request, "favlist.html", {"favorited_apartments": favorited_apartments}
     )
+    
+    def get_context_data(self, **kwargs):
+        context = super(LocationView, self).get_context_data(**kwargs)
+        context['form'] = ReviewForm
+        return context
+
+
+@login_required
+def review(request, pk):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            r = Review(
+                user = request.user,
+                location = Location.objects.only("id").get(id=pk),
+                content = request.POST['content']
+            )
+            r.save()
+        return HttpResponseRedirect(reverse("location", args=(pk,)))
--- review/views.py
+++ review/views.py
@@ -1,18 +1,1 @@
-from mainapp.models import SiteUser
-from location.models import Location
-from review.models import Review
-
-from .models import Review
-
-from .form import ReviewForm
-
-
-def review(request):
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-
-    else:
-        form = ReviewForm()
-
-
+# review/views.py
