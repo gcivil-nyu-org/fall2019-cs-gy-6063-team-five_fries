239555f8431f0d47aed82b792973a9f2cc80ee2e
Yonguk Jeong <yonguk@nyu.edu>
2019-11-06 10:24:01 -0500

    Add more test cases for Zillow

--- external/zillow/stub.py
+++ external/zillow/stub.py
@@ -6,18 +6,25 @@ import os
 def fetch_zillow_housing(
     *, address, city_state=None, zipcode=None, show_rent_z_estimate
 ) -> List[Dict[str, any]]:
-    filepath = os.path.join(os.path.dirname(__file__), "sample-response.json")
+    filepath = os.path.join(
+        os.path.dirname(__file__), "sample-response/fetch_zillow_housing.json"
+    )
     with open(filepath) as f:
         return json.loads(f.read())
 
 
-def get_zillow_error_response() -> str:
-    filepath = os.path.join(os.path.dirname(__file__), "sample-response-error.xml")
+all_zillow_response_names = [
+    "zillow-normal.xml",
+    "zillow-error.xml",
+    "zillow-single.xml",
+]
+
+
+def get_zillow_response(name) -> str:
+    filepath = os.path.join(os.path.dirname(__file__), "sample-response", name)
     with open(filepath) as f:
         return f.read()
 
 
-def get_zillow_response() -> str:
-    filepath = os.path.join(os.path.dirname(__file__), "sample-response.xml")
-    with open(filepath) as f:
-        return f.read()
+def get_all_zillow_response() -> List[str]:
+    return [get_zillow_response(name) for name in all_zillow_response_names]
