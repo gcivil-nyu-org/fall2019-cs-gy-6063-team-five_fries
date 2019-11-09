50f1856138678427a768c0893232356d5bdd8240
Yonguk Jeong <yonguk@nyu.edu>
2019-11-09 16:20:44 -0500

    Update Apartment model by suit_num

--- external/cache/zillow.py
+++ external/cache/zillow.py
@@ -30,17 +30,33 @@ def refresh_zillow_housing(location):
             continue
 
         # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#update-or-create
-        Apartment.objects.update_or_create(
-            zpid=response.zpid,
-            defaults={
-                "estimated_rent_price": response.estimated_rent_price,
-                "estimated_rent_price_currency": response.estimated_rent_price_currency,
-                "last_estimated": response.last_estimated,
-                "zillow_url": response.url,
-                "suite_num": response_addr.get("suite_num", ""),
-                "location": location,
-            },
-        )
+        if response_addr.get("suite_num"):
+            apt, created = Apartment.objects.update_or_create(
+                suite_num=response_addr.get("suite_num"),
+                defaults={
+                    "zpid": response.zpid,
+                    "estimated_rent_price": response.estimated_rent_price,
+                    "last_estimated": response.last_estimated,
+                    "zillow_url": response.url,
+                    "location": location,
+                },
+            )
+        else:  # sometimes Zillow does not provide suite number
+            apt, created = Apartment.objects.update_or_create(
+                zpid=response.zpid,
+                defaults={
+                    "zpid": response.zpid,
+                    "estimated_rent_price": response.estimated_rent_price,
+                    "last_estimated": response.last_estimated,
+                    "zillow_url": response.url,
+                    "location": location,
+                    "suite_num": response_addr.get("suite_num"),
+                },
+            )
+
+        if created:
+            apt.rent_price = apt.estimated_rent_price
+            apt.save()
 
     location.last_fetched_zillow = timezone.now()
     location.save()
