6160b70a0305107d4f85468338275ae19a5fe194
muhammad_osama_khan <mok232@nyu.edu>
2019-11-10 17:29:13 -0500

    reformatted code to fix travis fail

--- citystreets/settings.py
+++ citystreets/settings.py
@@ -157,8 +157,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Crispy_Forms settings
 
