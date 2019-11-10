5a316b0c5201f32f8c61604c8531e4c3d75baaff
muhammad_osama_khan <mok232@nyu.edu>
2019-11-10 16:42:48 -0500

    created an ApartmentUploadForm using django forms

---
+++ location/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from localflavor.us import forms as us_forms
+
+class ApartmentUploadForm(forms.Form):
+    city = forms.CharField(label="City", max_length = 100)
+    state = us_forms.USStateField(label="State")
+    address = forms.CharField(label="Address", max_length=255)
+    zipcode = us_forms.USZipCodeField(label="Zip Code")
+    estimated_rent_price = forms.DecimalField(label="Rent Price ($)", max_digits=20, decimal_places=2)
+    latitude = forms.DecimalField(
+        label="Latitude", required = False, max_digits=9, decimal_places=6)
+    longitude = forms.DecimalField(
+        label="Longitude", required = False, max_digits=9, decimal_places=6)
+    suite_num = forms.CharField(label="Suite Number", required = False, max_length=30)
+
+
