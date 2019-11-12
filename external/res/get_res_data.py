ea75e1e522acb7605122c6593568bdb49596636b
Ayushi-0903 <ag7335@nyu.edu>
2019-11-12 09:38:55 -0500

    Added Res Feature

---
+++ external/res/get_res_data.py
@@ -0,0 +1,52 @@
+from sodapy import Socrata
+import requests
+import pandas as pd
+
+
+def get_res_data(zip, max_query_results=5, num_entries_to_search=10000, t_out=10):
+
+    res_dataset_domain = "https://data.cityofnewyork.us"
+    res_dataset_identifier = "43nn-pn8j"
+    res_dataset_token = None  # works with None but lower number of requests can be made
+    client = Socrata(res_dataset_domain, res_dataset_token)
+
+    client.timeout = t_out
+
+    results = None
+    query_results = None
+    timeout = False
+    no_matches = True
+
+    try:
+        results = client.get(
+            res_dataset_identifier,
+            select="dba, boro, zipcode, cuisine_description, action, violation_description, score",
+            # q=str(zip), #uncomment if want to query directly on the server side (may lead to timeout)
+            order="score DESC",
+            limit=num_entries_to_search,
+        )
+        results_df = pd.DataFrame.from_records(results)
+        results_df = results_df.loc[results_df["zipcode"] == str(zip)]
+        if len(results_df) > 0:
+            no_matches = False
+        if len(results_df) > max_query_results:
+            results_df = results_df[:max_query_results]
+        query_results = results_df.to_dict("records")
+    except requests.exceptions.Timeout:
+        timeout = True
+    return query_results, timeout, no_matches
+
+
+if __name__ == "__main__":
+    query_results, timeout, no_matches = get_res_data(10009)  # valid zip code
+    print(type(query_results))
+    print(len(query_results))
+    print(query_results)
+    print(timeout)
+    print(no_matches)
+    query_results, timeout, no_matches = get_res_data(100099)  # invalid zip code
+    print(type(query_results))
+    print(len(query_results))
+    print(query_results)
+    print(timeout)
+    print(no_matches)
