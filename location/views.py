8603ecc1c679a9614097af3b9f168f9f930c6495
muhammad_osama_khan <mok232@nyu.edu>
2019-11-15 21:07:22 -0500

    removed redundant import

--- location/views.py
+++ location/views.py
@@ -14,7 +14,6 @@ from django.contrib import messages
 from external.googleapi.fetch import fetch_geocode
 from external.googleapi import g_utils
 from django.core.mail import send_mail
-from django.shortcuts import redirect
 from django.conf import settings
 
 class LocationView(generic.DetailView):
