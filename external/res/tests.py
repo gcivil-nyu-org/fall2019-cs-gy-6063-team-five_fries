67bfbcdcffd2756f3c15e27d2460a56d320fa406
Ayushi-0903 <ag7335@nyu.edu>
2019-11-18 11:33:20 -0500

    Coverage 4

--- external/res/tests.py
+++ external/res/tests.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 from .fetch import fetch_res_data, fetch_res_data_as_dataframe, get_res_data
 from .stub import fetch_res_data as fetch_res_data_stub
-from .models import NYCResComplaint
+from .models import ResComplaint
 
 
-class FetchNYCResConstraintsTests(TestCase):
+class FetchResConstraintsTests(TestCase):
     @mock.patch("external.res.fetch.fetch_res_data", fetch_res_data_stub)
     def test_fetch_res_data_as_dataframe(self):
         df = fetch_res_data_as_dataframe("11201")
@@ -17,7 +17,7 @@ class FetchNYCResConstraintsTests(TestCase):
     def test_get_res_data(self):
         results = get_res_data("11201")
         for complaint in results:
-            self.assertTrue(isinstance(complaint, NYCResComplaint))
+            self.assertTrue(isinstance(complaint, ResComplaint))
 
     @mock.patch("external.res.fetch.Socrata")
     def test_fetch_res_data(self, MockSocrata):  # noqa: N803
