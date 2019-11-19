a288b66c78aed3d235fdfb7c479cd4d2c813e104
Yonguk Jeong <yonguk@nyu.edu>
2019-11-19 09:05:02 -0500

    More detailed usage

--- cli/normalize_us_address.py
+++ cli/normalize_us_address.py
@@ -6,8 +6,15 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 from external.googleapi import normalize_us_address  # noqa: E402
 
 usage = """
+What it does:
+    Normalize an address to the standard form. It handles upper/lowercase letter and a partial address, etc.
+
 usage:
-    python normalize_us_address.py "6 Jay St"
+    python normalize_us_address.py "6 metrotech"
+
+output:
+    Address(street='6 MetroTech Center', zipcode='11201', city='Brooklyn', state='NY', latitude=40.6942793, longitude=-73.98649809999999)
+    6 MetroTech Center, Brooklyn, NY, 11201
 """
 
 
