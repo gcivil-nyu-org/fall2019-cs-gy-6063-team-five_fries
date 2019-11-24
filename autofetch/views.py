0fc4fd5590cc2aa451b6e2c4a9ae25594459f434
KShih <kontonsine@gmail.com>
2019-11-23 19:25:57 -0500

    fix apartment updated logic error

--- autofetch/views.py
+++ autofetch/views.py
@@ -61,72 +61,74 @@ def bckgrndfetch(city_list, limit):
 
         for r in results:
 
-            if len(Apartment.objects.filter(c_id=r["id"])) > 0:
-                continue
-
-            # check if is_existed w/ address reversed from lat,lon
-            if r["geotag"] is not None:
-
-                address, city, state, zipcode, full_address = "", "", "NY", 11201, ""
-
-                lat = r["geotag"][0]
-                lon = r["geotag"][1]
-
-                try:
-                    reverse_response = fetch_reverse_geocode((lat, lon))
-
-                except googlemaps.exceptions.TransportError:
-                    print(googlemaps.exceptions.TransportError)
-                    continue
-
-                if "formatted_address" in reverse_response[0].keys():
-                    full_address = reverse_response[0]["formatted_address"]
-
-                normalize_addr_dic = normalize_us_address(full_address)
-
-                if not normalize_addr_dic:
-                    print("return None from nomalize_us_address")
-                    print(f"The address input is: {full_address}")
-                    continue
-
-                state = normalize_addr_dic.state
-                address = normalize_addr_dic.street
-                city = normalize_addr_dic.city
-                zipcode = normalize_addr_dic.zipcode
-
-            else:
-                print("No geotag in craiglist results")
-                continue
-
-            # match the not null constraint of postgre
-            if state is None or address is None or city is None or zipcode is None:
-                print("state, address, city or zipcode is None")
-                continue
-
-            loc, loc_created = Location.objects.get_or_create(
-                address=address, city=city, state=state, zipcode=zipcode
-            )
-
-            if loc_created:
-                loc.latitude = lat
-                loc.longitude = lon
-                loc.save()
-
             c_id = r["id"]
             price = int(r["price"].split("$")[1])
             last_updated = r["last_updated"]
             bedrooms = r["bedrooms"]
 
-            apartment, apa_created = Apartment.objects.get_or_create(
-                c_id=c_id, location=loc
-            )
+            if len(Apartment.objects.filter(c_id=c_id)) > 0:
 
-            if apa_created:
-                apartment.rent_price = price
-                apartment.number_of_bed = bedrooms
-                apartment.last_modified = last_updated
-                apartment.save()
-            elif apartment.last_modified != last_updated:
+                apartment = Apartment.objects.get(c_id=c_id)
+                if apartment.last_modified != last_updated:
+                    apartment.rent_price = price
+                    apartment.number_of_bed = bedrooms
+                    apartment.last_modified = last_updated
+                    apartment.save()
+
+            # Apartment not exist, create a location for it
+            else:
+                # check if is_existed w/ address reversed from lat,lon
+                if r["geotag"] is not None:
+
+                    address, city, state, zipcode, full_address = "", "", "NY", 11201, ""
+
+                    lat = r["geotag"][0]
+                    lon = r["geotag"][1]
+
+                    try:
+                        reverse_response = fetch_reverse_geocode((lat, lon))
+
+                    except googlemaps.exceptions.TransportError:
+                        print(googlemaps.exceptions.TransportError)
+                        continue
+
+                    if "formatted_address" in reverse_response[0].keys():
+                        full_address = reverse_response[0]["formatted_address"]
+
+                    normalize_addr_dic = normalize_us_address(full_address)
+
+                    if not normalize_addr_dic:
+                        print("return None from nomalize_us_address")
+                        print(f"The address input is: {full_address}")
+                        continue
+
+                    state = normalize_addr_dic.state
+                    address = normalize_addr_dic.street
+                    city = normalize_addr_dic.city
+                    zipcode = normalize_addr_dic.zipcode
+
+                else:
+                    print("No geotag in craiglist results")
+                    continue
+
+                # match the not null constraint of postgre
+                if state is None or address is None or city is None or zipcode is None:
+                    print("state, address, city or zipcode is None")
+                    continue
+
+                loc, loc_created = Location.objects.get_or_create(
+                    address=address, city=city, state=state, zipcode=zipcode
+                )
+
+                if loc_created:
+                    loc.latitude = lat
+                    loc.longitude = lon
+                    loc.save()
+
+                apartment, apa_created = Apartment.objects.get_or_create(
+                    c_id=c_id, location=loc
+                )
+
                 apartment.rent_price = price
                 apartment.number_of_bed = bedrooms
                 apartment.last_modified = last_updated
