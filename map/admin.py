10790f767c42bfde43fb3d36d878b10d72c37ecf
ab7289 <ab7289@nyu.edu>
2019-12-05 19:36:46 -0500

    removed dead map app

--- map/admin.py
+++
@@ -1,7 +0,0 @@
-# from django.contrib import admin
-# from .models import CityStreetSpot
-
-# admin.site.register(CityStreetSpot)
-# # enable drag-drop marker in admin page:
-# # from leaflet.admin import LeafletGeoAdmin
-# # admin.site.register(CityStreetSpot, LeafletGeoAdmin)
