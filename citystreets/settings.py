d9572c1b3bee66423355286c5f8fa9d2e83ed626
KShih <kontonsine@gmail.com>
2019-12-06 16:54:48 -0500

    add whitenoise, TODO: fix marker-shadow

--- citystreets/settings.py
+++ citystreets/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # add whitenoise
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
