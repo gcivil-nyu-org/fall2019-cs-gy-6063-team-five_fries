98d7d2da9eee739040163ffb2bede7137741239c
KShih <kontonsine@gmail.com>
2019-12-07 20:16:56 -0500

    fix upload display on search page

--- search/views.py
+++ search/views.py
@@ -92,7 +92,7 @@ def search(request):
                 matching_apartments[loc.id] = num_matches
                 for apt in matches:
                     if apt.image:
-                        thumbnails[loc.id] = apt.image
+                        thumbnails[loc.id] = apt.picture_url
                         break
 
             search_data["matching_apartments"] = matching_apartments
